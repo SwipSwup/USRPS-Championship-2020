db3d2aade46b0f46d2ba34323032b9293f881e3d
SwipSwup <davidveigel.business@gmail.com>
2021-06-14 17:03:14 +0200

    Sketchy fix for core module

--- src/php/addRound.php
+++ src/php/addRound.php
@@ -21,39 +21,71 @@ $playerOneSign = $_GET['sign1'];
 $playerTwoSign = $_GET['sign2'];
 $champNr = $_GET['championship'];
 
-if (empty($playerOne)) {
-    echo "Please provide a name for this Player";
-}
-
-if (empty($playerTwo)) {
-    echo "Please provide a name for this Player";
-}
-
 $gameCounter = $conn->createQueryBuilder();
 
 $gameCounter = $conn->executeQuery("SELECT * FROM game")->rowCount();
 
+$pNames = $conn->createQueryBuilder()
+    ->select("pk_gamertag")
+    ->from("player")
+    ->executeQuery()->fetchAllAssociative();
 
+$tmp = array();
+foreach ($pNames as $item) {
+    array_push($tmp, $item["pk_gamertag"]);
+}
+
+if (!in_array($playerOne, $tmp)) {
+    $conn->createQueryBuilder()
+        ->insert("player")
+        ->values(
+            array(
+                "pk_gamertag" => '?',
+                "birthday" => '?'
+            ))
+        ->setParameter(0, $playerOne)
+        ->setParameter(1, 'null')
+        ->executeQuery();
+}
+
+if (!in_array($playerTwo, $tmp)) {
+    $conn->createQueryBuilder()
+        ->insert("player")
+        ->values(
+            array(
+                "pk_gamertag" => '?',
+                "birthday" => '?'
+            ))
+        ->setParameter(0, $playerTwo)
+        ->setParameter(1, 'null')
+        ->executeQuery();
+}
 
 $addRound
     ->insert('game')
     ->values(
         array(
-            'pk_gameNr' => $gameCounter + 1,
-            'fk_champNr' => $champNr,
-            'fk_player1' => $playerOne,
-            'fk_player2' => $playerTwo,
-            'signP1' => $playerOneSign,
-            'signP2' => $playerTwoSign,
-            'time' => '\'11:11:20\''
+            'pk_gameNr' => '?',
+            'fk_champNr' => '?',
+            'fk_player1' => '?',
+            'fk_player2' => '?',
+            'signP1' => '?',
+            'signP2' => '?',
+            'time' => '?'
         )
-    );
-
+    )
+    ->setParameter(0, $gameCounter + 1)
+    ->setParameter(1, $champNr)
+    ->setParameter(2, $playerOne)
+    ->setParameter(3, $playerTwo)
+    ->setParameter(4, $playerOneSign)
+    ->setParameter(5, $playerTwoSign)
+    ->setParameter(6, '\'11:11:20\'');
 
 
 $addRound->executeQuery();
 
-header("Location: http://localhost:81/WEBT/USRPS-Championship-2020/index.php", true, 301);
+header("Location: http://localhost/USRPS-Championship-2020/index.php", true, 301);
 exit();
 
 
